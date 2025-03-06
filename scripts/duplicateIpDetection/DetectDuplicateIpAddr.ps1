e7e247fce323ad04ea3dc12a9316c82599ad4142
Rishu Singh <rishusingh@microsoft.com>
2025-03-06 14:48:34 +0530

    Adding HNS trace collection ability to catch the dulpicate endpoint creation.

--- scripts/duplicateIpDetection/DetectDuplicateIpAddr.ps1
+++ scripts/duplicateIpDetection/DetectDuplicateIpAddr.ps1
@@ -6,8 +6,27 @@ Write-Host "Trying to load HNS module..."
 
 ipmo $BaseDir\hns.v2.psm1 -Force | Write-Host
 
+$iter = 1
+
+pktmon stop # Stopping if pktmon is already running
+
+# Start pktmon
+Write-Host "Starting pktmon with trace level 6 for HNS"
+pktmon start --capture --comp all --trace -p Microsoft-Windows-Host-Network-Service -l 6 -f traces.etl --pkt-size 100 -s 1024
+
 while($true){
-    $ipAddresses = ((Get-HnsEndpoint).IpConfigurations).IpAddress
+    $ipAddresses = @(
+    "192.168.1.1",
+    "192.168.1.2",
+    "192.168.1.4",
+    "192.168.1.4",
+    "192.168.1.5",
+    "192.168.1.5",
+    "192.168.1.7",
+    "192.168.1.8",
+    "192.168.1.9",
+    "192.168.1.10"
+)#((Get-HnsEndpoint).IpConfigurations).IpAddress
 
     Write-Host "IP addresses on the node:"
     foreach($ip in $ipAddresses){
@@ -21,25 +40,32 @@ while($true){
         break
     }
 
+    $iter++
+
     Start-Sleep -Seconds 300
 }
 
 Write-Host "Duplicate IP addresses found on the node, Duplicate IP addresses are:"
 
 foreach($ipGroup in $duplicateIpAddr){
-    foreach($ip in $ipGroup.Group){
-        Write-Host $ip
-    }
+    Write-Host $ipGroup.Name
 }
+
+Write-Host "Stopping pktmon..."
+# Stop pktmon
+pktmon stop
 
 Write-Host "Collecting Windows logs..."
 $collectWindowsLogs = "$BaseDir\collect-windows-logs.ps1" 
 powershell $collectWindowsLogs | Write-Host
 
-Write-Host "Collected Windows logs are at $PWD"
+Write-Host "Collected Windows logs and trace are at $PWD"
 
-Write-Host "Issue has been detected, going into infinite loop to keep the container running..."
 while($true){
-    Write-Host "Container is running in infinte while..."
+    if ($iter -Eq 1) {
+		Write-Host "The issue was detected in the first iteration and it may have happened already, and log rotation may have occurred."
+	} else {
+		Write-Host "Issue detected. Please download and review the collected Windows logs and also traces from the following path: $PWD\traces.etl"
+	}
     Start-Sleep -Seconds 3600
 }
